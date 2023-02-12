d0f18fb2cc7db881c225c0943d940b1a01122ab3
Kartavya <patelkartu951@gmail.com>
2023-02-12 14:22:14 +0530

    Hackathon with flask

---
+++ app.py
@@ -0,0 +1,25 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+model = pickle.load(open("mode.pkl","rb"))
+
+@app.route("/")
+def home():
+    return "Hrl"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    raw_data = request.get_json()
+    df = pd.json_normalize(raw_data)
+    df.to_csv('2.csv', index=False)
+    data = pd.read_csv("2.csv")
+    prediction = model.predict(data)
+    kar = pd.read_csv("FinalData - FinalData.csv")
+    kar2 = kar.loc[kar["Row Labels"] == prediction[0]].values[0]
+    return kar2[1]
+
+if __name__ ==  "__main__":
+    app.run(debug=True)
